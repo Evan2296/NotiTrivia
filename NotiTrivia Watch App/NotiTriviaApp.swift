526b628068129364ea84d6fef1741d512229ef45
Evan2296 <129124905+Evan2296@users.noreply.github.com>
2026-04-18 19:37:07 -0400

    Initial Commit

---
+++ NotiTrivia Watch App/NotiTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiTriviaApp.swift
+//  NotiTrivia Watch App
+//
+//  Created by Evan Levinsky on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct NotiTrivia_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
